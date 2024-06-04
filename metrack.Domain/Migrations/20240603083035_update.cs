4e96e1d145897b6a8bfd60e042677667eb38a40d
Tkachev.Nikita <n.o.tkachev@urfu.ru>
2024-06-04 10:23:10 +0500

    update migrations

---
+++ metrack.Domain/Migrations/20240603083035_update.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace metrack.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Issues_Stages_IssueStageId",
+                table: "Issues");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Issues_IssueStageId",
+                table: "Issues");
+
+            migrationBuilder.DropColumn(
+                name: "IssueStageId",
+                table: "Issues");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Issues");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "StageId",
+                table: "Issues",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_StageId",
+                table: "Issues",
+                column: "StageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Issues_Stages_StageId",
+                table: "Issues",
+                column: "StageId",
+                principalTable: "Stages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Issues_Stages_StageId",
+                table: "Issues");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Issues_StageId",
+                table: "Issues");
+
+            migrationBuilder.DropColumn(
+                name: "StageId",
+                table: "Issues");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "IssueStageId",
+                table: "Issues",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Issues",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_IssueStageId",
+                table: "Issues",
+                column: "IssueStageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Issues_Stages_IssueStageId",
+                table: "Issues",
+                column: "IssueStageId",
+                principalTable: "Stages",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ metrack.Domain/Migrations/20240603131044_update2.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace metrack.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class update2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Issues_Stages_StageId",
+                table: "Issues");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Issues_Users_OwnerId",
+                table: "Issues");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Issues_OwnerId",
+                table: "Issues");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Issues_StageId",
+                table: "Issues");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Issues");
+
+            migrationBuilder.DropColumn(
+                name: "StageId",
+                table: "Issues");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Issues");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Issues",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "Period",
+                table: "Issues",
+                newName: "CreatedAt");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Issues",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Issues",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Photo",
+                table: "Issues",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Issues");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Issues");
+
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "Issues");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Issues",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Issues",
+                newName: "Period");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId",
+                table: "Issues",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "StageId",
+                table: "Issues",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Issues",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_OwnerId",
+                table: "Issues",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_StageId",
+                table: "Issues",
+                column: "StageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Issues_Stages_StageId",
+                table: "Issues",
+                column: "StageId",
+                principalTable: "Stages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Issues_Users_OwnerId",
+                table: "Issues",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
